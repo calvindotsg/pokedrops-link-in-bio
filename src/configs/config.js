59216b94d02b247ed0a8f80d8b9868a55909ebe5
Calvin <68703834+calvindotsg@users.noreply.github.com>
2025-02-08 15:29:29 +0800

    Update config.js add products

--- src/configs/config.js
+++ src/configs/config.js
@@ -3,6 +3,7 @@ import profileImage from '../images/profilepic.jpg?url';
 import backgroundImage from '../images/backgrounds/pokedrop.jpg?url';
 import productImage from '../images/product1.jpg?url';
 import productImage20250207 from '../images/product20250207.JPG?url';
+import productImage20250208 from '../images/product20250208.JPG?url';
 
 /** @type {import('../types.js').SiteConfig} */
 const config = {
@@ -26,10 +27,10 @@ const config = {
             title: "Pokémon UPC 151",
             description: "151 ultra premium collection, Great condition",
             url: "https://www.carousell.sg/p/good-deal-pokemon-upc-151-1350900199/",
-            price: 300,
+            price: 330,
             includePriceOnSite: true,
             image: productImage,
-            availability: "Reserved", // Options: Available, Reserved, Sold
+            availability: "Available", // Options: Available, Reserved, Sold
         }, {
             title: "[PALDEA EVOLVED] Sleeved Booster Packs",
             description: "Selling Leftover PALDEA EVOLVED sleeved boosters. HAVE 30 LEFT. $8.20 / pack, $8 / pack for all",
@@ -38,7 +39,15 @@ const config = {
             includePriceOnSite: true,
             image: productImage20250207,
             availability: "Available" // Options: Available, Reserved, Sold
-        },// Add more products as needed
+        }, {
+            title: "Super Electric Breaker JP BB",
+            description: "[Non-shrinked] have 2 boxes, 85 per box, 83 per box if buy both",
+            url: "https://carousell.app.link/MQf9CmXkOQb",
+            price: 83,
+            includePriceOnSite: true,
+            image: productImage20250208,
+            availability: "Available" // Options: Available, Reserved, Sold
+        }, // Add more products as needed
     ],
 };
 
